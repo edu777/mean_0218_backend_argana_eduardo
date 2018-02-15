64630842e5e54be02b1b6d65c6dce510cafc8f91
edyargana777 <edyargana777@gmail.com>
2018-02-15 17:18:09 -0400

    Rutas modulares

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 
 var express = require('express');
 var app = express();
+var usersRoute = require('./routes/users.route');
 
     app.get('/',function (request, response) {
     response.send('Hola mundo');
     });
+    
+    app.use('/users', usersRoute);
 
-    app.get('/',function (request, response) {
-    app.route('/usuarios')
-    .get(function (request, response) {
-        response.send('accediendo a usuarios con el metodo get');
-      });
-
-    app.post(function (request, response) {
-    })
-    .post(function (request, response) {
-        response.send('accediendo a usuarios con el metodo get');
-    });
-    
-    app.put(function (request, response) {
-    })
-    .put(function (request, response) {
-        response.send('accediendo a usuarios con el metodo get');
-    });
-    
-    app.delete(function (request, response) {
-    })
-    .delete(function (request, response) {
-        response.send('accediendo a usuarios con el metodo get');
-    });
-    
-    
-    });
-    
     app.listen(3000,function () {
         console.log('corriendo en el puerto 3000');
       }); 
